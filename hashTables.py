300a0f0f06cc2d3b7fc181270cc7fe3de27bbb28
jenny-morin <jennifer.l.morin@maine.edu>
2025-11-18 10:42:24 -0500

    Original Commit 11/18
    Original Commit:
    MOCK_DATA.csv included
    Work on hashTables.py
    -Data Item created
    -Hash Function is ascii data of the movies name
    -Also included is code that figures out the maximum key for the data set (used for size of the table)
    -If there is a data collision Linear probing is used
    -Data loading into the file is included

---
+++ hashTables.py
@@ -0,0 +1,97 @@
+import csv as csv
+#Hash Function- create a good method
+
+#Linear probing, if full insert in next avaiable spot(could loop back to the top)
+    #Stop searching when you find an empty spot
+#Linked list when a collide happens you append it 
+size = 59494 
+hashTable = [None] * size
+x=0
+biggest = 0
+
+class DataItem:
+    def __init__(self, line):
+        self.movie_name = line[0]
+        self.genre = line[1]
+        self.release_data = line[2]
+        self.director = line[3]
+        self.revenue = float(line[4][1:])
+        self.rating = float(line[5])
+        self.min_duration  = int(line[6])
+        self.production_comp  = line[7]
+        self.quote  = line[8]
+        #print(self.movie_name,self.genre,self.release_data,self.director,self.revenue,self.rating,self.min_duration,self.production_comp,self.quote)
+
+
+def hashFunction(mydata,x,biggest):
+    
+    ascii_values=[]
+    for char in mydata.movie_name:
+        ascii_values += [ord(char)]
+    key = 0
+    for i in ascii_values:
+        key+=i
+    if x==0:
+        biggest = key
+        x+=1
+    elif key > biggest:
+        biggest = key
+    return key,x,biggest
+    #print(key) 
+    #print(ascii(mydata.movie_name))
+def hashInsert(key, mydata):
+    if hashTable[key] == None:
+        hashTable[key]= mydata
+        print(hashTable[key])
+    else:
+        print("I am not adding anythinggg")
+        hashCollision(key,mydata)
+
+def hashCollision(key,mydata):
+    ogKey=key
+    currKey = hashTable[key]
+    while currKey != None:
+        key += 1
+        if ogKey == key:
+            print("Full")
+            return
+        currKey = hashTable[key]
+    print("put her there")
+    hashTable[key]=mydata
+    #hashInsert(key,mydata)
+    
+
+
+    
+
+
+file = "MOCK_DATA.csv"
+counter =0
+
+with open(file, 'r', newline='', encoding ="utf8") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        #print(row)
+        if counter != 0:
+        #create a DataItem from row
+            myDataItem = DataItem(row)
+            titleKey,x,biggest = hashFunction(myDataItem,x,biggest)
+            #print(titleKey)
+            hashInsert(titleKey,myDataItem)
+        print(biggest)
+            
+        #feed the appropriate feild into the hash function to get a key
+        #mod the key value by the hash table length
+        #try to insert DataItem into hash table
+        #handle any collisions
+        counter+=1
+print(counter)
+
+
+#handle collision function that decides on a new key value or adds it as the linked list
+
+#reflection is in readme
+#use csv library for python
+
+#[0]*1500
+#prime numbers
