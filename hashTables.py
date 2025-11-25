49abba7365fe5cfdc2b72366061eb4e7b57aee87
jenny-morin <jennifer.l.morin@maine.edu>
2025-11-25 09:58:01 -0500

    HashTable Optimization Attempt 5
    -This Optimization utilized the linked list method of storing variables in the hash table
    -In order to create a key for each data item I first added the ascii values of every character in the title/quote, then I squared the sum and converted it into a string, then after figuring out the length of the string I cut the first fourth and the last fourth out and reconvert the middle half back into an integer
    -In addition to the changes in my hash function I also modded the key by the size of the list in order to prevent out of index issues
    -I choose this method because in my previous methods I prefered the use of a linked list for its easier ability to search for an item but in other attempts I ended up with a lot of empty space particularly when using the quote. With both the help of a more precise hashing function and the introduction of modding the key by the size will hopefully reduce the amount of unused memory.
    The results using the movie titles were:
    Collisions: 13795
    Empty Spots: 8787
    Time used: 0.24 Seconds
    The results using the movie quotes were:
    Collisions: 13386
    Empty Spots: 8378
    Time used: 0.28 Seconds

--- hashTables.py
+++ hashTables.py
@@ -10,12 +10,12 @@ import time
     #Stop searching when you find an empty spot
 #Linked list when a collide happens you append it 
 #size = 59494 
-size=621062
-hashTable = [None] * size
+#size=200000000
 # x=0
 # biggest = 0
 collision = 0
 itemsPlaced = 0
+#hashTable = [None] * size
 
 class DataItem:
     def __init__(self, line):
@@ -33,22 +33,28 @@ class DataItem:
 
 def hashFunction(mydata):
     ascii_values=[]
-    for char in mydata.quote:
     #for char in mydata.quote:
+    for char in mydata.movie_name:
         ascii_values += [ord(char)]
     key = 0
     for i in ascii_values:
         key+=i
-    key = key//7
+    key = key*key
+    key = str(key)
+    length = len(key)
+    length=length//4
+    endLength=length*3
+    key=key[length:endLength]
+    key=int(key)
     return key
 
 def hashInsert(key, mydata,collision,itemsPlaced):
     if hashTable[key] == None:
         hashTable[key]= mydata
-        #itemsPlaced+=1
+        itemsPlaced+=1
     else:
-        collision=hashCollision(key,mydata,collision)
-    itemsPlaced+=1
+        collision=hashCollisionLinked(key,mydata,collision)
+    #itemsPlaced+=1
     return collision,itemsPlaced
 
 def hashCollisionLinked(key,mydata,collision):
@@ -64,15 +70,18 @@ def hashCollisionLinked(key,mydata,collision):
 
 def hashCollision(key,mydata,collision):
     ogKey=key
-    print(key)
+    #print(key)
     currKey = hashTable[key]
     while currKey != None:
-        key += 1
-        if ogKey == key:
-            print("Full")
-            return
+        if key+1<size:
+            key += 1
+            if ogKey == key:
+                print("Full")
+                return
+        else:
+            key = 0
         currKey = hashTable[key]
-    print(key)
+    #print(key)
     hashTable[key]=mydata
     collision+=1
     return collision
@@ -80,17 +89,35 @@ def hashCollision(key,mydata,collision):
 file = "MOCK_DATA.csv"
 lineCounter =0
 start = time.time()
+with open(file, 'r', newline='', encoding ="utf8") as myFile:
+    reader = csv.reader(myFile)
+    for row in reader:
+        if lineCounter != 0:
+            myDataItem = DataItem(row)
+            titleKey= hashFunction(myDataItem)
+            if lineCounter==1:
+                biggest = titleKey
+            elif titleKey>biggest:
+                biggest=titleKey
+        lineCounter+=1
+myFile.close()
+size = biggest
+
+hashTable = [None] * (size+300000)
+lineCounter=0
+
 with open(file, 'r', newline='', encoding ="utf8") as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         if lineCounter != 0:
             myDataItem = DataItem(row)
             titleKey= hashFunction(myDataItem)
+            titleKey=titleKey%size
             collision,itemsPlaced=hashInsert(titleKey,myDataItem,collision,itemsPlaced)
         lineCounter+=1
 end = time.time()
 
-print(f"\n---Hash Table using the movie quotes---")
+print(f"\n---Hash Table using the movie title---")
 print(f"\tLines read: {lineCounter}")
 print(f"\tItems added: {lineCounter-1}")
 print(f"\tCollisions handled: {collision}")
